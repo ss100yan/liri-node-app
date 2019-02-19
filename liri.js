1807cdf99e5993b965466e5064abb72e680c5c49
ss100yan <32798618+ss100yan@users.noreply.github.com>
2019-02-19 16:18:00 -0500

    new

--- liri.js
+++ liri.js
@@ -11,38 +11,46 @@ var axios = require("axios");
 
 var liriOption = process.argv[2]; 
 var UserInput = process.argv[3];
-// -----------------axios
-
-axios.get("https://rest.bandsintown.com/artists/" +UserInput + "/events?app_id=codingbootcamp")
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
- 
-  //------------------Spotify
 
 
 
+
+  UserInputs (liriOption, UserInput);
+  
 
 
   //------------------Switch statemant
-
-  switch(expression) {
-    case x:
+  function UserInputs (liriOption, UserInput){
+  switch(liriOption) {
+    case 'concert-this':
+      bands(UserInput);
+      break;
+    case 'spotify-this-song':
       // code block
       break;
-    case y:
+      case 'movie-this':
+      // code block
+      break;
+      case 'do-what-it-says':
       // code block
       break;
     default:
-      // code block
+    console.log("Invalid Option. Try: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says")
   }
 
+  }
 
-
-  // concert-this
-  // spotify-this-song
-  // movie-this
-  // do-what-it-says
+  // -----------------axios
+  function bands(UserInput){
+    var queryUrl = "https://rest.bandsintown.com/artists/" + UserInput + "/events?app_id=codingbootcamp";
+    axios.get(queryUrl)
+      .then(function (response) {
+        console.log(response);
+        console.log(UserInput)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+      //------------------Spotify
+    
