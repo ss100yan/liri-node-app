e23bc36558e1e168092a2eeddebf4dfb4eced8f7
ss100yan <32798618+ss100yan@users.noreply.github.com>
2019-02-19 00:43:27 -0500

    new

--- liri.js
+++ liri.js
@@ -1,6 +1,24 @@
 require("dotenv").config();
 
 
+//-------------- variables
 
-var spotify = new Spotify(keys.spotify);
+
 var keys = require('./keys.js');
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+
+var liriOption = process.argv[2]; 
+var UserInput = process.argv[3];
+// -----------------axios
+
+axios.get("https://rest.bandsintown.com/artists/" +UserInput + "/events?app_id=codingbootcamp")
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+ 
+  //------------------Spotify
