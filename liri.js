a2e0e3cac3e5fac726471e8805630339fc6be74e
ss100yan <32798618+ss100yan@users.noreply.github.com>
2019-02-21 00:23:52 -0500

    new

--- liri.js
+++ liri.js
@@ -42,64 +42,99 @@ var UserInput = process.argv[3];
 
   // --------------------Axios
 
-  //--------------------Bands in town
+                //--------------------Bands in town
   function bands(UserInput){
     var queryUrl = "https://rest.bandsintown.com/artists/" + UserInput + "/events?app_id=codingbootcamp";
     axios.get(queryUrl)
       .then(function (response) {
-        console.log(response);
-        console.log(UserInput)
+       
+        console.log("**********Events for"+" "+ UserInput +"*********");
+      
+         var concerts= response;
+
+        for (var i = 0; i < concerts.data.length; i++) {  
+
+          console.log(i+1);
+
+          console.log("Venue Name: " +concerts.data[i].venue.name);
+                          
+          console.log("Venue Location: " +concerts.data[i].venue.city);
+        
+          console.log("Date of the Event: "+concerts.data[i].datetime);  //  (use moment to format this as "MM/DD/YYYY")
+        
+               
+        }
       })
       .catch(function (error) {
         console.log(error);
       });
     }
-                                           //     Name of the venue
-                                           // Venue location
-                                           // Date of the Event (use moment to format this as "MM/DD/YYYY")
-    //--------------------OMBD
+   
+
+                       
+
+            //--------------------OMDB
     function omdb(UserInput){
-      var queryUrl = " http://www.omdbapi.com/?t=" + UserInput + " i=tt3896198&apikey=ed4f19eb";
+
+      if (UserInput==null){UserInput='Mr. Nobody.'}
+
+
+      var queryUrl = " http://www.omdbapi.com/?t=" + UserInput + " &y=&plot=short&apikey=ed4f19eb";
+     
+
       axios.get(queryUrl)
         .then(function (response) {
-          console.log(response);
-          console.log(UserInput)
+             
+          console.log("**********Movie Info for"+" "+ UserInput +"*********");
+
+          console.log("Title: " + response.data.Title);
+
+          console.log("Release Year: " + response.data.Year);
+
+          console.log("IMDB Rating: " + response.data.imdbRating);
+          
+          console.log("Rotten Tomatoes Rating: " + response.data.Ratings); // ???????
+          
+          console.log("Country of production: " + response.data.Country);
+          
+          console.log("Language: " + response.data.Language);
+          
+          console.log("Plot: " + response.data.Plot);
+
+          console.log("Actors: " + response.data.Actors);
+
         })
         .catch(function (error) {
           console.log(error);
         });
       }
-                                                            // * Title of the movie.
-                                                            // * Year the movie came out.
-                                                            // * IMDB Rating of the movie.
-                                                               // * Rotten Tomatoes Rating of the movie.
-                                                            // * Country where the movie was produced.
-                                                                // * Language of the movie.
-                                                            // * Plot of the movie.
-                                                               // * Actors in the movie.
-   
-     
-                                                              //  If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+                                                                    
+                                          //function to get proper Rotten Tomatoes Rating
+                            //  function getRottenTomatoesRatingObject (data) {
+                            //  return data.Ratings.find(function (item) {
+                            //     return item.Source === "Rotten Tomatoes";
+                            //       });
+                            //       }
 
-
-                                                              //  If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
-                                                               
-                                                              //  It's on Netflix!
+                            //      function getRottenTomatoesRatingValue (data) {
+                            //     return getRottenTomatoesRatingObject(data).Value;
+                                //  }                              
      
-                                                               //------------------Spotify
+     //------------------Spotify
+
       function spfy(UserInput){
       spotify.search({ type: 'track', query: UserInput  }, function(err, data) {
         if (err) {
           return console.log('Error occurred: ' + err);
         }
+     console.log("**********Spotify Info for the song-'"+ UserInput+  "'*********");
+      console.log('Artist(s) -'+" "+ data.tracks.items[1].album.artists[0].name); 
+      console.log("The song's name -"+" "+ data.tracks.items[1].album.artists[0].name); 
+      console.log('A preview link of the song from Spotify -'+" "+ data.tracks.items[1].album.artists[0].name); 
+      console.log('The album that the song is from -'+" "+ data.tracks.items[1].album.artists[0].name); 
       
-      console.log(data); 
       });
     }
-                                                                     // Artist(s)
-                                                                     // The song's name
-                                                                     // A preview link of the song from Spotify
-                                                                     // The album that the song is from
 
 
     // ------------------------------------fs
